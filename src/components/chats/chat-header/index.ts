70e78e772bbb5831cf2ad8d7c99f714961e61d09
DIMA <Pecherskiy@vector-best.ru>
2024-04-09 10:21:49 +0700

    bugfix img

--- src/components/chats/chat-header/index.ts
+++ src/components/chats/chat-header/index.ts
@@ -1,2 +1,16 @@
+import Handlebars from 'handlebars';
+
 import './chat-header.css';
 export { default as ChatHeader } from './chat-header.hbs?raw';
+
+import ava from '../../../../src/assets/01.jpg'
+import menu from '../../../../src/assets/icons/menu.svg'
+
+Handlebars.registerHelper("img_ava", function () {
+    return ava;
+});
+
+Handlebars.registerHelper("img_menu", function () {
+    return menu;
+});
+
